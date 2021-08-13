4190f73e7c4fe0902249c243f1b1d46a3adac234
Mun Kit <leoan96@hotmail.com>
2021-08-13 22:09:30 +0800

    setup basic main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { CustomLogger } from './logger/custom-logger.logger';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useLogger(app.get(CustomLogger));
+
+  const config = app.get(ConfigService);
+
+  const port = +config.get<String>('SERVER_PORT');
+  await app.listen(port);
+  logger.log(`Server running on port ${port}...`);
 }
 bootstrap();
+
+process.on('uncaughtException', (error) => {
+  logger.error(
+    `UNCAUGHT EXCEPTION - keeping process alive:\n ${
+      error.stack
+    }\n ${JSON.stringify(error)}`,
+  );
+});
