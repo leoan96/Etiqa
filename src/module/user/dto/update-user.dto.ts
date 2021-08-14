a6b8b71634ec2c115e75a3580b9091a52fa72fd6
Mun Kit <leoan96@hotmail.com>
2021-08-14 23:35:26 +0800

    enchance create & update user dto with swagger @ApiProperty

--- src/module/user/dto/update-user.dto.ts
+++ src/module/user/dto/update-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsEmail,
@@ -8,25 +9,46 @@ import {
 } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({ description: 'username', type: 'string', example: 'john' })
   @IsString()
   @Length(3, 16)
   @IsOptional()
   username?: string;
 
+  @ApiProperty({
+    description: 'email',
+    type: 'string',
+    example: 'john@gmail.com',
+  })
   @IsEmail()
   @MaxLength(50)
   @IsOptional()
   email?: string;
 
+  @ApiProperty({
+    description: 'phone number',
+    type: 'string',
+    example: '920-291-2140',
+  })
   @IsString()
   @Length(11, 12)
   @IsOptional()
   phoneNumber?: string;
 
+  @ApiProperty({
+    description: 'skillsets',
+    type: 'string',
+    example: ['java', 'typescript'],
+  })
   @IsArray()
   @IsOptional()
   skillsets?: string[];
 
+  @ApiProperty({
+    description: 'hobby',
+    type: 'string',
+    example: ['swimming', 'reading'],
+  })
   @IsArray()
   @IsOptional()
   hobby?: string[];
