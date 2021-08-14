ac9d71ca6f87dbc3f5e916163192b6c4d5bf0a69
Mun Kit <leoan96@hotmail.com>
2021-08-14 15:51:09 +0800

    setup mongoose connection

---
+++ src/module/schema/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ toObject: { getters: true }, versionKey: false })
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  phoneNumber: string;
+
+  @Prop({ required: true })
+  skillsets: [string];
+
+  @Prop({ default: 'en' })
+  hobby: [string];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
