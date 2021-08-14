2d2e929a7c682e23b6653a173c0103553eee62fb
Mun Kit <leoan96@hotmail.com>
2021-08-14 22:35:44 +0800

    implement logging interceptor

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,17 @@ import { AllExceptionFilter } from './filter/http-exception.filter';
 import { MongoExceptionFilter } from './filter/mongodb-exception.filter';
 import { CustomLogger } from './logger/custom-logger.logger';
 import * as express from 'express';
+import * as httpContext from 'express-http-context';
+import { LoggingInterceptor } from './interceptor/logging.interceptor';
+import { setCorrelationId } from '../shared/utils';
 
 const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useLogger(app.get(CustomLogger));
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   const config = app.get(ConfigService);
 
@@ -20,6 +25,8 @@ async function bootstrap() {
   app.useGlobalFilters(new MongoExceptionFilter());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(httpContext.middleware);
+  app.use(setCorrelationId);
 
   const port = +config.get<String>('SERVER_PORT');
   await app.listen(port);
