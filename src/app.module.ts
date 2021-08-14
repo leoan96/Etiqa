b6ee20b7d43e63a364c6432776a93c7b08a1ca48
Mun Kit <leoan96@hotmail.com>
2021-08-14 15:51:09 +0800

    setup mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CustomLogger } from './logger/custom-logger.logger';
+import { mongooseEnvironmentConfiguration } from './module/mongoose/mongoose.configuration';
+import { MongooseClient } from './module/mongoose/mongoose.provider';
+import { UserModule } from './module/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-      load: [],
+      load: [mongooseEnvironmentConfiguration],
       isGlobal: true,
     }),
-    CustomLogger
+    MongooseModule.forRootAsync(MongooseClient),
+    CustomLogger,
+    UserModule
   ],
 })
 export class AppModule {}
