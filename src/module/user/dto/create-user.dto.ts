56b6b96b3600e52421bd7d8b58df1a55978fe731
Mun Kit <leoan96@hotmail.com>
2021-08-14 22:19:58 +0800

    implement class validator for update & create user dto

--- src/module/user/dto/create-user.dto.ts
+++ src/module/user/dto/create-user.dto.ts
@@ -1,8 +1,34 @@
+import {
+  IsArray,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Length,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 16)
+  @IsOptional()
   username: string;
+
+  @IsEmail()
+  @MaxLength(50)
+  @IsOptional()
   email: string;
+
+  @IsString()
+  @Length(11, 12)
+  @IsOptional()
   phoneNumber: string;
+
+  @IsArray()
+  @IsOptional()
   skillsets: string[];
+
+  @IsArray()
+  @IsOptional()
   hobby: string[];
 }
 
